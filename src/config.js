d26dc0fc6821d67007027fa3d0735a06edc0f167
Anders Ramsay <819213+andersr@users.noreply.github.com>
2019-12-22 12:51:11 -0500

    Add amplify config

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "andersco-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://jwqg8o6s85.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_i4A7QLUF9",
+      APP_CLIENT_ID: "3m9v2p3099vtooru9gmtf4u0i2",
+      IDENTITY_POOL_ID: "us-east-1:e01210fc-2c1a-42c7-ae30-783cd5e0febd"
+    }
+  };
