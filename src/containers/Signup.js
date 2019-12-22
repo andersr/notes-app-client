67a59da1ea688f7446df6d50bea11c84a66f2e6c
Anders Ramsay <819213+andersr@users.noreply.github.com>
2019-12-22 15:50:31 -0500

    Add signup with verification

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -8,6 +8,7 @@ import {
 import LoaderButton from "../components/LoaderButton";
 import { useFormFields } from "../libs/hooksLib";
 import "./Signup.css";
+import { Auth } from "aws-amplify";
 
 export default function Signup(props) {
   const [fields, handleFieldChange] = useFormFields({
@@ -31,20 +32,39 @@ export default function Signup(props) {
     return fields.confirmationCode.length > 0;
   }
 
-  async function handleSubmit(event) {
+async function handleConfirmationSubmit(event) {
     event.preventDefault();
-
+  
     setIsLoading(true);
-
-    setNewUser("test");
-
-    setIsLoading(false);
+  
+    try {
+      await Auth.confirmSignUp(fields.email, fields.confirmationCode);
+      await Auth.signIn(fields.email, fields.password);
+  
+      props.userHasAuthenticated(true);
+      props.history.push("/");
+    } catch (e) {
+      alert(e.message);
+      setIsLoading(false);
+    }
   }
 
-  async function handleConfirmationSubmit(event) {
+async function handleSubmit(event) {
     event.preventDefault();
-
+  
     setIsLoading(true);
+  
+    try {
+      const newUser = await Auth.signUp({
+        username: fields.email,
+        password: fields.password
+      });
+      setIsLoading(false);
+      setNewUser(newUser);
+    } catch (e) {
+      alert(e.message);
+      setIsLoading(false);
+    }
   }
 
   function renderConfirmationForm() {
