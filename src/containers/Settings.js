643ddd5af8c97fd4f9b873257d019807bb888af7
Anders Ramsay <819213+andersr@users.noreply.github.com>
2019-12-24 12:18:30 -0500

    Add purchasing abiilty

--- src/containers/Settings.js
+++ src/containers/Settings.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { API } from "aws-amplify";
-
+import { StripeProvider, Elements } from "react-stripe-elements";
+import BillingForm from "../components/BillingForm";
+import config from "../config";
 export default function Settings(props) {
   const [isLoading, setIsLoading] = useState(false);
 
@@ -10,8 +12,38 @@ export default function Settings(props) {
     });
   }
 
+  async function handleFormSubmit(storage, { token, error }) {
+    if (error) {
+      alert(error);
+      return;
+    }
+  
+    setIsLoading(true);
+  
+    try {
+      await billUser({
+        storage,
+        source: token.id
+      });
+  
+      alert("Your card has been charged successfully!");
+      props.history.push("/");
+    } catch (e) {
+      alert(e);
+      setIsLoading(false);
+    }
+  }
+  
   return (
     <div className="Settings">
+      <StripeProvider apiKey={config.STRIPE_KEY}>
+        <Elements>
+          <BillingForm
+            isLoading={isLoading}
+            onSubmit={handleFormSubmit}
+          />
+        </Elements>
+      </StripeProvider>
     </div>
   );
 }
